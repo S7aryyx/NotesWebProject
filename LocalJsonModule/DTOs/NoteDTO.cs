ef3ddd73e2775b9ede0c55e06c7610d28cd70070
S7aryyx <Ahmetovilgam18@gmail.com>
2026-09-13 03:17:54 +0500

    Deleted unused files.

--- LocalJsonModule/DTOs/NoteDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace LocalJsonModule.DTOs;
-
-public class NoteDTO
-{
-    public string Title { get; set; } = string.Empty;
-    public string Content { get; set; } = string.Empty;
-    public Guid OwnerId { get; set; }
-}
